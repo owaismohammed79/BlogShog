684f8a014a4e6f1bda9182a2b8f1774c1669a993
Mohammed Owais <owaismohammed795@gmail.com>
2025-05-13 21:43:21 +0530

    Improved UI for postcard

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -18,12 +18,12 @@ function Postcard({
 
   return (//Realize ki sab ko Link tag ke andar enclose kia h
     <Link to={`/posts/${$id}`}>
-        <div className='w-full h-72 bg-gray-100 rounded-xl  flex flex-col justify-between'>
+        <div className='w-full h-80 bg-gray-100 rounded-xl  flex flex-col justify-between'>
             <div className='h-2/3 rounded-xl flex'>
                 {/*Idhar filePreview fn asks for fileId jisme ki featuredImage ki Id bhej rahe he kyunki uska preview chahiye na hame*/}
                 <img src={previewObject?.href} alt ={title} className='rounded-t-xl w-full object-cover'/>
             </div>
-            <div className='text-xl font-bold text-center flex flex-col justify-center my-3'>
+            <div className='h-1/3 text-2xl font-bold flex flex-col justify-center items-center'>
                 {title}
             </div>
         </div>
