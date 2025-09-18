c44aebbf0c2cbcc015ffab871660924bf3c868e4
Mohammed Owais <owaismohammed795@gmail.com>
2025-09-18 17:08:24 +0530

    Added vercel speed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {login, logout} from "./store/authSlice"
 import { Outlet} from "react-router-dom"
 import Loading from "./components/ui/Loading.jsx"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
           <Outlet />
         </main>
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </div>
     </div>
